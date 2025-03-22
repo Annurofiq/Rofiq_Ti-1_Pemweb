cdcbdb4ecdd7ac3bb1416b5effe9316d8d942945
Annurofiq <rofiqcrb75@gmail.com>
2025-03-22 10:20:33 +0700

    Pertemuan 2

---
+++ hello-react/src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Header extends React.Component {
+    render(){
+      return (
+        <div>
+          <h2>Absen yang buka puasa nya sama Steik  </h2>
+        </div>
+      )
+    }
+  }
+
+  export default Header
